dc62bdff0f97cf8c292ef06a6aabca348bb7e614
m1cha1s <msulej04@gmail.com>
2023-01-30 16:00:17 +0100

    Raylib works!

--- build.zig
+++ build.zig
@@ -1,19 +1,20 @@
 const std = @import("std");
+const raylib = @import("libs/raylib/lib.zig");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
     const exe = b.addExecutable("boids-zig", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
+
+    const system_lib = b.option(bool, "system-raylib", "link to preinstalled raylib library") orelse false;
+
+    raylib.link(exe, system_lib);
+    raylib.addAsPackage("raylib", exe);
+    raylib.math.addAsPackage("raymath", exe);
+
     exe.install();
 
     const run_cmd = exe.run();
--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,22 @@
 const std = @import("std");
+const rl = @import("raylib");
 const print = std.debug.print;
 
-pub fn main() !void {}
+const width = 800;
+const height = 800;
+
+pub fn main() !void {
+    rl.InitWindow(width, height, "Boids");
+    defer rl.CloseWindow();
+
+    rl.SetTargetFPS(60);
+
+    while (!rl.WindowShouldClose()) {
+        rl.BeginDrawing();
+        defer rl.EndDrawing();
+
+        rl.ClearBackground(rl.BLACK);
+
+        rl.DrawText("Hello world!", 100, 100, 20, rl.RAYWHITE);
+    }
+}
